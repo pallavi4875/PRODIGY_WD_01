f8ccf059a511942ee14d2a5a594cfbaeaeae8d6b
pallavi4875 <pallaviyadav4875@gmail.com>
2023-10-30 19:05:01 +0530

    Responsive landing page

---
+++ script.js
@@ -0,0 +1,14 @@
+function navItem1() {
+    document.getElementById("navItem1").style.color = "red";
+    document.getElementById("navItem2").style.color = " #323f4b";
+    document.getElementById("navItem3").style.color = " #323f4b";
+    document.getElementById("navItem4").style.color = " #323f4b";
+}
+
+function navItem2() {
+    document.getElementById("navItem2").style.color = "red";
+    document.getElementById("navItem1").style.color = " #323f4b";
+    document.getElementById("navItem3").style.color = " #323f4b";
+    document.getElementById("navItem4").style.color = " #323f4b";
+
+}
